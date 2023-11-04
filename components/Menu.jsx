ed3ce3631b1237810970df9c553fec69a3aadd4d
Asraful Alam <asrafulalamazad@gmail.com>
2023-11-05 00:00:55 +0600

    loader and active link css

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,16 +1,20 @@
+"use client"
 import React from 'react';
-
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 const Menu = () => {
+    const currentPath=usePathname();
+
     return (
         <div>
-                <Link href={"/"}>Home</Link><br/>
-                <Link href={"/profile"}>Profile</Link><br/>
-                <Link href={"/about"}>About</Link><br/>
-                <Link href={"/product"}>Product</Link><br/>
-                <Link href={"/products"}>Products</Link><br/>
-                <Link href={"/products/server-site"}>Products</Link>
+            <h1>{currentPath}</h1> <br/>
+                <Link  className={currentPath==="/"?"active-link":"pending-link"}  href={"/"}>Home</Link><br/>
+                <Link  className={currentPath==="/profile"?"active-link":"pending-link"} href={"/profile"}>Profile</Link><br/>
+                <Link  className={currentPath==="/about"?"active-link":"pending-link"} href={"/about"}>About</Link><br/>
+                <Link  className={currentPath==="/product"?"active-link":"pending-link"} href={"/product"}>Product</Link><br/>
+                <Link  className={currentPath==="/products"?"active-link":"pending-link"} href={"/products"}>Products</Link><br/>
+                <Link  className={currentPath==="/products/server-site"?"active-link":"pending-link"} href={"/products/server-site"}>Products</Link>
 
         </div>
     );
