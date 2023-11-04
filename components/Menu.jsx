5f308d8dd52230dba5ddcb847c8d35bdc23cb2e8
Asraful Alam <asrafulalamazad@gmail.com>
2023-11-05 00:12:12 +0600

    query parameter wiht pathname using usePathname

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -9,11 +9,14 @@ const Menu = () => {
     return (
         <div>
             <h1>{currentPath}</h1> <br/>
+         {/*//   <h2>{{pathname:"currentPath",query:{name:"book",price:"300"}}}</h2>*/}
                 <Link  className={currentPath==="/"?"active-link":"pending-link"}  href={"/"}>Home</Link><br/>
                 <Link  className={currentPath==="/profile"?"active-link":"pending-link"} href={"/profile"}>Profile</Link><br/>
                 <Link  className={currentPath==="/about"?"active-link":"pending-link"} href={"/about"}>About</Link><br/>
                 <Link  className={currentPath==="/product"?"active-link":"pending-link"} href={"/product"}>Product</Link><br/>
-                <Link  className={currentPath==="/products"?"active-link":"pending-link"} href={"/products"}>Products</Link><br/>
+                <Link  className={currentPath==="/products"?"active-link":"pending-link"}
+                       href={{pathname:"/currentPath",query:{name:"book",price:"300"}}}>
+                           Products</Link><br/>
                 <Link  className={currentPath==="/products/server-site"?"active-link":"pending-link"} href={"/products/server-site"}>Products</Link>
 
         </div>
